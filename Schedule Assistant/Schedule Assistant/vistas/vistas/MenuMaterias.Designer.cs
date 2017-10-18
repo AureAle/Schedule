3e9d632df647c029ac51b0cb443db11bf7fe2b67
AureAle <32805589+AureAle@users.noreply.github.com>
2017-10-18 00:08:11 -0500

    Add files via upload
    nuevos nombres

---
+++ Schedule Assistant/Schedule Assistant/vistas/vistas/MenuMaterias.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Schedule_Assistant
+{
+    partial class MenuMaterias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.bttnBorrarMateria = new System.Windows.Forms.Button();
+            this.lstListaMateriasProfesores = new System.Windows.Forms.ListBox();
+            this.lblMateria = new System.Windows.Forms.Label();
+            this.bttnAgregarMaterias = new System.Windows.Forms.Button();
+            this.lblMaterias = new System.Windows.Forms.Label();
+            this.txtCreditos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bttnAgregarRelacion = new System.Windows.Forms.Button();
+            this.bttnEliminarRelacion = new System.Windows.Forms.Button();
+            this.lstListaMaterias = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMateria.ForeColor = System.Drawing.Color.SteelBlue;
+            this.txtMateria.Location = new System.Drawing.Point(191, 200);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(292, 26);
+            this.txtMateria.TabIndex = 19;
+            // 
+            // bttnBorrarMateria
+            // 
+            this.bttnBorrarMateria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnBorrarMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnBorrarMateria.ForeColor = System.Drawing.Color.SteelBlue;
+            this.bttnBorrarMateria.Location = new System.Drawing.Point(123, 544);
+            this.bttnBorrarMateria.Name = "bttnBorrarMateria";
+            this.bttnBorrarMateria.Size = new System.Drawing.Size(146, 32);
+            this.bttnBorrarMateria.TabIndex = 18;
+            this.bttnBorrarMateria.Text = "Eliminar Materia";
+            this.bttnBorrarMateria.UseVisualStyleBackColor = true;
+            // 
+            // lstListaMateriasProfesores
+            // 
+            this.lstListaMateriasProfesores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstListaMateriasProfesores.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lstListaMateriasProfesores.FormattingEnabled = true;
+            this.lstListaMateriasProfesores.ItemHeight = 20;
+            this.lstListaMateriasProfesores.Location = new System.Drawing.Point(518, 354);
+            this.lstListaMateriasProfesores.Name = "lstListaMateriasProfesores";
+            this.lstListaMateriasProfesores.Size = new System.Drawing.Size(146, 144);
+            this.lstListaMateriasProfesores.TabIndex = 17;
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMateria.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblMateria.Location = new System.Drawing.Point(119, 204);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(66, 20);
+            this.lblMateria.TabIndex = 16;
+            this.lblMateria.Text = "Materia:";
+            // 
+            // bttnAgregarMaterias
+            // 
+            this.bttnAgregarMaterias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnAgregarMaterias.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnAgregarMaterias.ForeColor = System.Drawing.Color.SteelBlue;
+            this.bttnAgregarMaterias.Location = new System.Drawing.Point(337, 544);
+            this.bttnAgregarMaterias.Name = "bttnAgregarMaterias";
+            this.bttnAgregarMaterias.Size = new System.Drawing.Size(146, 32);
+            this.bttnAgregarMaterias.TabIndex = 15;
+            this.bttnAgregarMaterias.Text = "Agregar Materia";
+            this.bttnAgregarMaterias.UseVisualStyleBackColor = false;
+            // 
+            // lblMaterias
+            // 
+            this.lblMaterias.AutoSize = true;
+            this.lblMaterias.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaterias.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblMaterias.Location = new System.Drawing.Point(240, 106);
+            this.lblMaterias.Name = "lblMaterias";
+            this.lblMaterias.Size = new System.Drawing.Size(161, 42);
+            this.lblMaterias.TabIndex = 20;
+            this.lblMaterias.Text = "Materias";
+            // 
+            // txtCreditos
+            // 
+            this.txtCreditos.Location = new System.Drawing.Point(536, 200);
+            this.txtCreditos.Name = "txtCreditos";
+            this.txtCreditos.Size = new System.Drawing.Size(100, 26);
+            this.txtCreditos.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(546, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Creditos:";
+            // 
+            // bttnAgregarRelacion
+            // 
+            this.bttnAgregarRelacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnAgregarRelacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnAgregarRelacion.ForeColor = System.Drawing.Color.SteelBlue;
+            this.bttnAgregarRelacion.Location = new System.Drawing.Point(518, 252);
+            this.bttnAgregarRelacion.Name = "bttnAgregarRelacion";
+            this.bttnAgregarRelacion.Size = new System.Drawing.Size(146, 32);
+            this.bttnAgregarRelacion.TabIndex = 23;
+            this.bttnAgregarRelacion.Text = "Agregar Relación";
+            this.bttnAgregarRelacion.UseVisualStyleBackColor = false;
+            // 
+            // bttnEliminarRelacion
+            // 
+            this.bttnEliminarRelacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnEliminarRelacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnEliminarRelacion.ForeColor = System.Drawing.Color.SteelBlue;
+            this.bttnEliminarRelacion.Location = new System.Drawing.Point(518, 299);
+            this.bttnEliminarRelacion.Name = "bttnEliminarRelacion";
+            this.bttnEliminarRelacion.Size = new System.Drawing.Size(146, 32);
+            this.bttnEliminarRelacion.TabIndex = 24;
+            this.bttnEliminarRelacion.Text = "Eliminar Relación";
+            this.bttnEliminarRelacion.UseVisualStyleBackColor = true;
+            // 
+            // lstListaMaterias
+            // 
+            this.lstListaMaterias.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstListaMaterias.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lstListaMaterias.FormattingEnabled = true;
+            this.lstListaMaterias.ItemHeight = 20;
+            this.lstListaMaterias.Location = new System.Drawing.Point(123, 274);
+            this.lstListaMaterias.Name = "lstListaMaterias";
+            this.lstListaMaterias.Size = new System.Drawing.Size(360, 224);
+            this.lstListaMaterias.TabIndex = 25;
+            // 
+            // MenuMaterias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(783, 683);
+            this.Controls.Add(this.lstListaMaterias);
+            this.Controls.Add(this.bttnEliminarRelacion);
+            this.Controls.Add(this.bttnAgregarRelacion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCreditos);
+            this.Controls.Add(this.lblMaterias);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.bttnBorrarMateria);
+            this.Controls.Add(this.lstListaMateriasProfesores);
+            this.Controls.Add(this.lblMateria);
+            this.Controls.Add(this.bttnAgregarMaterias);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.SteelBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MenuMaterias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.Button bttnBorrarMateria;
+        private System.Windows.Forms.ListBox lstListaMateriasProfesores;
+        private System.Windows.Forms.Label lblMateria;
+        private System.Windows.Forms.Button bttnAgregarMaterias;
+        private System.Windows.Forms.Label lblMaterias;
+        private System.Windows.Forms.TextBox txtCreditos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bttnAgregarRelacion;
+        private System.Windows.Forms.Button bttnEliminarRelacion;
+        private System.Windows.Forms.ListBox lstListaMaterias;
+    }
+}
