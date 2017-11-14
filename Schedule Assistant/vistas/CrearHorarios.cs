00d6f49cd9fd0c9ec3368655b13490aa1d2e8fdd
Aure Ale <aurenargle@hotmail.com>
2017-11-14 17:51:25 -0600

    colors

--- Schedule Assistant/vistas/CrearHorarios.cs
+++ Schedule Assistant/vistas/CrearHorarios.cs
@@ -72,6 +72,11 @@ namespace Schedule_Assistant.vistas
             }
 
             ClaseSelec = sender as botonClase;
+            for(int i= 0; i< Conector.leerHorasNoDisponiblesDe(ClaseSelec.Clase.Profesor).Length; i++)
+            {
+                this.tablePanelHorairo.GetControlFromPosition(Conector.leerHorasNoDisponiblesDe(ClaseSelec.Clase.Profesor)[i].Dia-1,
+                    Conector.leerHorasNoDisponiblesDe(ClaseSelec.Clase.Profesor)[i].Hora1 - 1).BackColor = Color.IndianRed;
+            }
             //colorear las horas en las que no pueda ir el maestro
         }
 
